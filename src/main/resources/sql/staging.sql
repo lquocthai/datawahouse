4539f940b81092030731e6c7f85cc1cb676f2035
22130243VoDaiThanhTai <22130243@st.hcmuaf.edu.vn>
2025-11-19 22:32:59 +0700

    fix data_dim

--- src/main/resources/sql/staging.sql
+++ src/main/resources/sql/staging.sql
@@ -21,26 +21,16 @@ CREATE TABLE raw_data (
 
 -- Bảng DATE_DIM
 CREATE TABLE date_dim (
-                          date_sk INT PRIMARY KEY,             -- 1
-                          full_date DATE,                      -- 2
-                          day_since_2005 INT,                  -- 3
-                          month_since_2005 INT,                -- 4
-                          day_of_week VARCHAR(20),             -- 5
-                          calendar_month VARCHAR(20),          -- 6
-                          calendar_year INT,                   -- 7
-                          calendar_year_month VARCHAR(20),     -- 8
-                          day_of_month INT,                    -- 9
-                          day_of_year INT,                     -- 10
-                          week_of_year_sunday INT,             -- 11
-                          year_week_sunday VARCHAR(10),        -- 12
-                          week_sunday_start DATE,              -- 13
-                          week_of_year_monday INT,             -- 14
-                          year_week_monday VARCHAR(10),        -- 15
-                          week_monday_start DATE,              -- 16
-                          quarter_year VARCHAR(10),            -- 17  (vd: 2005-Q01)
-                          quarter_since_2005 INT,              -- 18
-                          holiday VARCHAR(20),                 -- 19
-                          day_type VARCHAR(20)                 -- 20
+                          date_key INT PRIMARY KEY,
+                          full_date DATE NOT NULL,
+                          day INT,
+                          month INT,
+                          quarter INT,
+                          year INT,
+                          day_name VARCHAR(20),
+                          month_name VARCHAR(20),
+                          is_weekend BOOLEAN
+
 );
 -- Bảng transformed_data: Dùng để lưu dữ liệu đã được transform từ bảng raw_data
 CREATE TABLE transformed_data (
